5105c9dfe746880ae993b378a851b94aea74c652
zhouliangshun <liangshunzhou@gmail.com>
2016-11-07 14:36:54 +0800

    fixed some bugs
    Signed-off-by: zhouliangshun <liangshunzhou@gmail.com>

---
+++ src/test/java/com/kylins/obj/pref/SharePrefTest.java
@@ -0,0 +1,16 @@
+package com.kylins.obj.pref;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by developer on 04/11/2016.
+ */
+public class SharePrefTest {
+    @Test
+    public void getInstance() throws Exception {
+
+    }
+
+}
